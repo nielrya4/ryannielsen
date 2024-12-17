0f076aeadc12d7bc5c7951b2f01f655e3d0765c7
nielrya4 <nielrya4@isu.edu>
2024-12-17 13:44:25 -0700

    Automated commit on 2024-12-17

--- include/js/collapsible.js
+++ include/js/collapsible.js
@@ -3,8 +3,7 @@ var i;
 
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
-        this.classList.add("collapsible")
-        this.classList.toggle("active");
+        this.classList.toggle("active_collapsible");
         var content = this.nextElementSibling;
         if (content.style.maxHeight){
             content.style.maxHeight = null;
